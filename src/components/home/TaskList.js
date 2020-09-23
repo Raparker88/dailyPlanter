98c52148281e0f4581c87a5df42765841919791e
Raparker88 <raparker88@gmail.com>
2020-09-23 18:35:00 -0500

    styling

--- src/components/home/TaskList.js
+++ src/components/home/TaskList.js
@@ -14,84 +14,105 @@ export const TaskList = (props) => {
     useEffect(() => {
         getCrops()
         getScheduledPlantings()
-    } ,[])
+    }, [])
 
     useEffect(() => {
         const weekInMilliseconds = 7 * 24 * 60 * 60 * 1000
         const thisWeek = scheduledPlantings.filter(p => {
-            if(p.date >= Date.now() && p.date <= Date.now()+weekInMilliseconds
-             && p.userId === parseInt(localStorage.getItem("seedPlan_user")) || new Date(p.date).toDateString() === new Date(Date.now()).toDateString()){
+            if (p.date >= Date.now() && p.date <= Date.now() + weekInMilliseconds
+                && p.userId === parseInt(localStorage.getItem("seedPlan_user")) || new Date(p.date).toDateString() === new Date(Date.now()).toDateString()) {
                 return true
             }
         }) || []
         //sort in ascending order
-        const sortedFuture = thisWeek.sort(function(a,b){return a.date-b.date}) || []
+        const sortedFuture = thisWeek.sort(function (a, b) { return a.date - b.date }) || []
         setFutureSchedule(sortedFuture)
 
         const pastOverdue = scheduledPlantings.filter(p => {
-            if(p.date < Date.now() && new Date(p.date).toDateString() != new Date(Date.now()).toDateString() && !p.complete){
+            if (p.date < Date.now() && new Date(p.date).toDateString() != new Date(Date.now()).toDateString() && !p.complete) {
                 return true
             }
         }) || []
         //sort in descending order
-        const sortedPast = pastOverdue.sort(function(a,b){return b.date-a.date}) || []
+        const sortedPast = pastOverdue.sort(function (a, b) { return b.date - a.date }) || []
         setPastSchedule(sortedPast)
-    },[scheduledPlantings])
+    }, [scheduledPlantings])
 
     const handleCheckbox = (eve) => {
         const checked = eve.target.checked
         const scheduleObj = scheduledPlantings.find(p => p.id === parseInt(eve.target.id)) || {}
-        if (checked){
+        if (checked) {
             scheduleObj.complete = true
             updateScheduledPlanting(scheduleObj)
-        }else{
+        } else {
             scheduleObj.complete = false
             updateScheduledPlanting(scheduleObj)
 
         }
     }
 
+    const isThereAFuture = () => {
+        if (futureSchedule.length === 0) {
+            return <p>Nothing scheduled at this time. Click schedule in the navbar to begin scheduling your planting season.</p>
+        }
+    }
+    const isOverdue = () => {
+        if (pastSchedule > 0) {
+            return (
+                <>
+                    <h2>Overdue</h2>
+                    <section className="plantings--container">
+                        {
+                            pastSchedule.map(ps => {
+                                const crop = crops.find(c => c.id === ps.cropId)
+                                return (
+                                    <div key={ps.id} className="detailCard--task">
+                                        <h4>{crop.name}</h4>
+                                        <p>{new Date(ps.date).toDateString()}</p>
+                                        <p>{ps.notes}</p>
+                                        <label htmlFor="pastComplete">Complete</label>
+                                        <input type="checkbox" id={ps.id} name="pastComplete" checked={ps.complete}
+                                            onChange={handleCheckbox}></input>
+
+                                    </div>
+                                )
+                            })
+                        }
+                    </section>
+                </>
+            )
+        }
+    }
+
+
     return (
         <>
-        <div className="homeContainer">
-            <h2>Scheduled Plantings This Week</h2>
-            <section className="plantings--container">
-                {futureSchedule.map(fs => {
-                    const crop = crops.find(c => c.id === fs.cropId)
-                    return (
-                        <div key={fs.id} className="detailCard--task">
-                            <h4>{crop.name}</h4>
-                            <p>{new Date(fs.date).toDateString()}</p>
-                            <p>{fs.notes}</p>
-                            <label htmlFor="complete">Complete</label>
-                            <input type="checkbox" id={fs.id} name="complete" checked={fs.complete}
-                                onChange={handleCheckbox}></input>
-                        </div>
-                    )
-                })}
-            </section>
-            <h2>Overdue</h2>
-            <section className="plantings--container">
-                {
-                    pastSchedule.map(ps => {
-                        const crop = crops.find(c => c.id === ps.cropId)
-                        return (
-                            <div key={ps.id} className="detailCard--task">
-                                <h4>{crop.name}</h4>
-                                <p>{new Date(ps.date).toDateString()}</p>
-                                <p>{ps.notes}</p>
-                                <label htmlFor="pastComplete">Complete</label>
-                                <input type="checkbox" id={ps.id} name="pastComplete" checked={ps.complete}
-                                    onChange={handleCheckbox}></input>
-                                
-                            </div>
-                        )
-                    })
-                }
-            </section>
-            <WeatherList/>
+            <div className="homeContainer">
+                <h2>Scheduled Plantings This Week</h2>
+                <section className="plantings--container">
+                    <div>{isThereAFuture()}</div>
 
-        </div>
+                    {futureSchedule.map(fs => {
+                        const crop = crops.find(c => c.id === fs.cropId)
+                        return (
+                            <>
+                                <div key={fs.id} className="detailCard--task">
+                                    <h4>{crop.name}</h4>
+                                    <p>{new Date(fs.date).toDateString()}</p>
+                                    <p>{fs.notes}</p>
+                                    <label htmlFor="complete">Complete</label>
+                                    <input type="checkbox" id={fs.id} name="complete" checked={fs.complete}
+                                        onChange={handleCheckbox}></input>
+                                </div>
+                            </>
+                        )
+                    })}
+                </section>
+                {isOverdue()}
+
+                <WeatherList />
+
+            </div>
         </>
     )
 }
