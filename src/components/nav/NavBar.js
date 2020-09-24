d4115fe90dbe844d6cc86dad03157904c1db7011
Raparker88 <raparker88@gmail.com>
2020-09-24 17:11:44 -0500

    logo on home page

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -5,6 +5,7 @@ import "./NavBar.css"
 export const NavBar = (props) => {
     return (
         <ul className="navbar">
+            
             <li className="navbar__item active">
                 <Link className="navbar__link" to="/home">Home</Link>
             </li>
